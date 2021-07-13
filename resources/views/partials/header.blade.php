e2b54b507813fbf5f4d59a56dad21dfb91ebd1d5
patriziosbr <sbrozzi.patrizio@gmail.com>
2021-07-13 17:03:15 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        
+        <link rel="stylesheet" href={{ asset("css/app.css") }}>
+        <title>Document</title>
+    </head>
+    <body>
+        <header>
+            <div class="bg-top">
+                <div class="bg-blue">
+                    <div>
+                        <p>DC POWER VISA &reg;</p>
+                    </div>
+                    <div class="additional">
+                        <p>ADDITIONAL SITE &#9660;</p>
+                    </div>
+                </div>
+            </div>
+
+            <nav>
+                <div class="logo">
+                    <img src="{{ asset("img/dc-logo.png") }}" alt="">
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="#">characters</a>
+                        </li>
+                        <li>
+                            <a class="active" href="#">comics</a>
+                        </li>
+                        <li>
+                            <a href="#">movies</a>
+                        </li>
+                        <li>
+                            <a href="#">tv</a>
+                        </li>
+                        <li>
+                            <a href="#">games</a>
+                        </li>
+                        <li>
+                            <a href="#">collectibles</a>
+                        </li>
+                        <li>
+                            <a href="#">videos</a>
+                        </li>
+                        <li>
+                            <a href="#">fans</a>
+                        </li>
+                        <li>
+                            <a href="#">news</a>
+                        </li>
+                        <li>
+                            <a href="#">shop <span>&#9660;</span></a>
+                        </li>
+                    </ul>
+                    </div>
+                    <div class="cerca">
+                        <p>Search</p>
+                    </div>
+            </nav>
+
+            <div class="jumbotron">
+                <img src=" {{ asset("img/jumbotron.jpg") }} " alt="">
+                <div class="container">
+                    <div class="absolute">
+                        <H5>current series</H5>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+    </body>
+</html>
